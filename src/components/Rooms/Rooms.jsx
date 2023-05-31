f414e5175b751388f4cbb6646cf5a4a71211e99d
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-31 21:16:32 +0600

    footer added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import { useSearchParams } from 'react-router-dom';
 import Container from '../shared/Container';
+import Loader from '../shared/Loader';
 import Card from './Card';
 
 const Rooms = () => {
 
+
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
     const [rooms, setRooms] = useState([])
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
+        setLoading(true)
         fetch('/rooms.json')
             .then(res => res.json())
-            .then(data => setRooms(data))
+            .then(data => {
+                if (category) {
+                    const filtered = data.filter(room => room.category === category)
+                    setRooms(filtered)
+                }
+                else {
+                    setRooms(data)
+                }
+                setLoading(false)
+            })
             .catch(err => toast.error(err.message));
-    }, [])
+    }, [category])
 
-
+    if (loading) {
+        return <Loader />
+    }
     return (
         <Container>
-            <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-7'>
+            <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10'>
                 {
                     rooms.map((room, index) => <Card key={index} room={room} ></Card>)
                 }
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../components/shared/Footer/Footer"
 import NavBar from "../components/shared/NavBar/NavBar"
 
 const Main = () => {
@@ -8,6 +9,7 @@ const Main = () => {
       <div className="pt-28 pb-20">
         <Outlet />  
       </div>
+      <Footer></Footer>
     </div>
   )
 }
